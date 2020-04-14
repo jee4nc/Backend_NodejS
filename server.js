95e9d0b30848e120489a6df29f16a088f7373864
Jean <jeanc.aldea1@gmail.com>
2020-04-13 20:33:10 -0400

    se crean rutas, get y post

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 // import express from 'express'; => esta es otra forma de importar el modulo de express ECMS6
 const express = require('express');
+const router = express.Router(); // permite poder separar get post u otras
+
 
 var app = express(); // Se inicializa express
 
-app.use('/', function(req, res){
-    res.send('Hola');
+// app.use('/', function(req, res){
+//     res.send('Hola');
+// });
+
+app.use(router);
+
+router.get('/', function (req, res) {
+    res.send('Hola desde GET');
+});
+
+router.post('/', function (req, res) {
+    res.send('Hola desde POST');
 });
 
 app.listen(3000);
