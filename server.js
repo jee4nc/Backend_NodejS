09a4afdefbdd51bbd09d32b6b4afda3b3206c20d
Jean <jeanc.aldea1@gmail.com>
2020-04-14 01:43:02 -0400

    Advance route and headers Creamos las rutas personalizadas y ocupamos express.json y express.url para parsear y ocupar metodo url.

--- server.js
+++ server.js
@@ -3,20 +3,32 @@ const express = require('express');
 const router = express.Router(); // permite poder separar get post u otras
 
 
-var app = express(); // Se inicializa express
-
 // app.use('/', function(req, res){
 //     res.send('Hola');
 // });
 
+var app = express(); // Se inicializa express
+app.use(express.json()); // Metodo util para parsear JSON
+app.use(express.urlencoded({extended: false})); // Permite este metodo de comunicacion
 app.use(router);
 
-router.get('/', function (req, res) {
-    res.send('Hola desde GET');
+router.get('/mensajes', function (req, res) {
+    console.log(req.headers);
+    res.header({ // Esto me permite poder enviar un header especifico
+                 // Esto se ve en el apartado Header
+        "custom-header": "Nuestro header",
+    });
+    res.send('Aqui iria una lista de mensajes');
 });
 
-router.post('/', function (req, res) {
-    res.send('Hola desde POST');
+router.post('/mensajes', function (req, res) {
+    res.send('Mensaje Añadido');
+});
+
+router.delete('/mensajes', function (req, res) {
+    console.log(req.query); // Esto permite que la url sea aceptado como body
+    console.log(req.body);  // Imprime el valor de body
+    res.send(`Mensaje ${req.body.text} Eliminado`);
 });
 
 app.listen(3000);
